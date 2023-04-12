545269c237c62cb0ff7b6c89af38f707752707c2
MrMario108 <m.majewski108@gmail.com>
2023-04-12 17:23:59 +0200

    update

---
+++ RealEstateSearch/olxSearch/tests/_models_withoutDB_.py
@@ -0,0 +1,94 @@
+import pytest
+from django.test import TestCase
+from scrapingApp.utils.abstractScraper import AbstractScraper, OLXCreateUrlsFromParameters
+from scrapingApp.utils.saveScraper import SaveApartment
+from olxSearch.models import SearchingSettings, Category, City, User
+from scrapingApp.tasks import GroupedSearchParameters
+from django.utils import timezone
+from django.utils.text import slugify
+
+@pytest.fixture
+def make_category(db):
+    def make(
+        categoryName: str = "Mieszkania",
+    ):
+        category = Category(
+            categoryName=categoryName
+        )
+        return category
+
+    return make
+
+@pytest.fixture
+def make_city():
+    def make(
+        cityName: str = "Warszawa",
+        slug: str = "warszawa"
+    ):
+        city = City(
+            cityName=cityName, slug=slugify(cityName)
+        )
+        return city
+
+    return make
+
+@pytest.fixture
+def make_user():
+    def make(
+        username: str = "admin",
+        password: str = "Polmrok0",
+        email: str = "test@example.com",
+        **rest
+    ):
+        user = User(
+            username=username, password=password, email=email, **rest
+        )
+        return user
+
+    return make
+
+@pytest.fixture
+def make_searchingSettings(make_user, make_city, make_category):
+    def make(title, category=None, user=None, city=None):
+        if user is None:
+            user = make_user()
+        if city is None:
+            city = make_city()
+        if category is None:
+            category = make_category()
+        
+        searchingSettings = SearchingSettings(
+            user=user,
+            city=city,
+            category=category,
+            title= title,
+            area=30,
+            rooms=1,
+            date_created = timezone.now(),
+            price = 4000
+        )
+        return searchingSettings
+
+    return make
+
+def test_searchingSettings(make_searchingSettings):
+    searchingSettings_1 = make_searchingSettings(title="Elaine")
+    assert searchingSettings_1.title == "Elaine"
+
+def test_searchingSettings_2(make_searchingSettings, make_category):
+    category = make_category(categoryName="Domy")
+    searchingSettings_1 = make_searchingSettings(title="Elaine", category = category)
+    assert searchingSettings_1.category.categoryName == "Domy"
+
+def test_findMaxPrice(make_category):
+    make_category(categoryName="Domy")
+    make_category(categoryName="Działki")
+    make_category(categoryName="Mieszkania")
+    assert "Mieszkania" in Category.objects.all()
+
+
+@pytest.fixture
+def city(db):
+    cities = ('Warszawa', 'Kraków', 'Łódź', 'Wrocław', 'Poznań', 'Gdańsk', 'Szczecin', 'Bydgoszcz', 'Lublin', 'Białystok', 'Katowice', 'Gdynia', 'Częstochowa', 'Radom', 'Rzeszów', 'Toruń', 'Sosnowiec', 'Kielce', 'Gliwice', 'Olsztyn', 'Zabrze', 'Bielsko-Biała', 'Bytom', 'Zielona Góra', 'Rybnik', 'Ruda Śląska', 'Opole', 'Tychy', 'Świętochłowice', 'Chorzów' )
+    for city in cities:
+        City.create(cityName=city)
--- RealEstateSearch/scrapingApp/tasks.py
+++ RealEstateSearch/scrapingApp/tasks.py
@@ -1,12 +1,47 @@
 from celery import shared_task
-from django.contrib.auth import get_user_model
-from django.core.mail import send_mail
-from realEstateSearch import settings as local
 
-@shared_task()
-def task_execute():
+from .utils.abstractScraper import AbstractScraper
+from .utils.saveScraper import SaveApartment
+from olxSearch.models import SearchingSettings, Category, City
+from django.db.models import Max
+from django.utils import settings
 
-    print("Hello from Celery")
+logger = logging.getLogger('celeryLogger')
+
+class GroupedSearchParameters():
+    
+    def get(self) -> list:
+        """ Groups parameters to limit the number of web pages scraped and the number of properties found """
+        groupedSearchParameters = []
+        category = Category.objects.all()
+        cities = City.objects.all()
+        
+        for cat in category:
+            for city in cities:
+                finded = self.findMaxPrice(cat, city)
+                if finded != None:
+                    groupedSearchParameters.append(finded)
+
+        logger.info("getGroupedSearchParameters; returned values: "+ str(groupedSearchParameters))
+
+        return groupedSearchParameters
+
+    def joinSingleSearchParametersFrom(self, selectedParameters, maxPrice):
+
+        selectedParameters[0]["price"] = maxPrice['price__max']
+        logger.info("joinSingleSearchParametersFrom: " + str(selectedParameters[0]))
+        return selectedParameters[0]
+
+    def findPricesForCategoryAndCity(self, category, city):
+        return SearchingSettings.objects.filter(category__categoryName = category.categoryName, city__slug = city.slug).values('area', 'rooms', 'price', 'city__slug', 'category__categoryName')
+
+    def findMaxPrice(self, category, city):
+        selectedParameters = self.findPricesForCategoryAndCity(category, city)
+        logger.info("getGroupedSearchParameters; selectedParameters: " + str(selectedParameters))
+        maxPrice = selectedParameters.aggregate(Max('price'))
+        logger.info("getGroupedSearchParameters; maxPrice: " + str(maxPrice))
+        if (maxPrice['price__max'] != None):
+            return self.joinSingleSearchParametersFrom(list(selectedParameters), maxPrice)
 
 
 @shared_task(bind=True)
